51874c204cbc2a3849696ee5fb0fd57cc305e2d0
mmaryamafzal490-creator <mmaryamafzal490@gmail.com>
2026-08-11 23:30:24 -0700

    Update career.js

--- api/career.js
+++ api/career.js
@@ -6,17 +6,17 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { message } = req.body;
+    const { interest, subject, skills, message } = req.body;
     const apiKey = process.env.GEMINI_API_KEY;
 
     if (!apiKey) {
       return res.status(500).json({ reply: "API Key missing in Vercel settings." });
     }
-
+const prompt = message ||'My interest is ${interest}, my favourite subject is ${subject}, and my skills are ${skills}. Please give me personalized career recommendations.';
     const genAI = new GoogleGenerativeAI(apiKey);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const result = await model.generateContent(message);
+    const result = await model.generateContent(prompt);
     const text = result.response.text();
 
     // Frontend expecting 'reply' or 'result' key
