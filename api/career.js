a90ef97a7126570b6377cee149eba60bbb04e94e
mmaryamafzal490-creator <mmaryamafzal490@gmail.com>
2026-08-12 00:20:20 -0700

    Update career.js

--- api/career.js
+++ api/career.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     }
 const prompt = message ||'My interest is ${interest}, my favourite subject is ${subject}, and my skills are ${skills}. Please give me personalized career recommendations.';
     const genAI = new GoogleGenerativeAI(apiKey);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     const result = await model.generateContent(prompt);
     const text = result.response.text();
